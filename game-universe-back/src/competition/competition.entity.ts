b510648e5b007bc7d82c8e3419153e7145c4a91e
Rached <archer193@hotmail.com>
2019-11-02 20:36:45 +0100

    Añadir validaciones

--- game-universe-back/src/competition/competition.entity.ts
+++ game-universe-back/src/competition/competition.entity.ts
@@ -1,13 +1,16 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { IsNotEmpty } from 'class-validator';
 
 @Entity('competition')
 export class CompetitionEntity {
     @PrimaryGeneratedColumn('increment')
     id: number
 
+    @IsNotEmpty()
     @Column('integer')
     account: number
 
+    @IsNotEmpty()
     @Column('character varying', { length: 50, unique: true })
     name: string
 
--- game-universe-back/src/competition/competition.service.ts
+++ game-universe-back/src/competition/competition.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { CompetitionEntity } from './competition.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CompetitionService extends TypeOrmCrudService<CompetitionEntity> {
-	constructor (@InjectRepository(CompetitionEntity) repo) {
+	constructor(@InjectRepository(CompetitionEntity) repo: Repository<CompetitionEntity>) {
 		super(repo)
-	} 
+	}
 }
